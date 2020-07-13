53d97275526777ecaa3ff5de6dd81a782ed7f48a
juancho108 <jjf108@gmail.com>
2020-07-13 18:53:44 -0300

    docs agregado

---
+++ docs/precache-manifest.f9d950eae85b67366848adc0b27edba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348d0cbadee224c00973d62828a00be1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c9831c708623a436a69",
+    "url": "/static/css/main.690fde00.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9831c708623a436a69",
+    "url": "/static/js/main.98a12670.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
